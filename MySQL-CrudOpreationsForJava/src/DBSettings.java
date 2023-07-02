af9e53ced4ae08b925bb1f7b43b66ea73cbc7a67
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-07-02 21:21:12 +0300

    Crud operations example added

---
+++ MySQL-CrudOpreationsForJava/src/DBSettings.java
@@ -0,0 +1,6 @@
+public class DBSettings
+{
+    public static final String URL="jdbc:mysql://localhost:3306/products";
+    public static final String userName="root";
+    public static final String password="emirEmir41";
+}
