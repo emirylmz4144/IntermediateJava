74bb43cba6a06d232e6737b87780036d8597c648
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-01-31 19:30:20 +0300

    BoxingGame Project was update

--- BoxingGame/src/Fighter.java
+++ BoxingGame/src/Fighter.java
@@ -7,6 +7,7 @@ public class Fighter
     int healty;
     int weight;
 
+    //Özellikler atanıyor
     public Fighter(String name, int demage, int healty, int weight)  //Constuructor oluşturuldu
     {
         this.name = name;
@@ -15,22 +16,22 @@ public class Fighter
         this.weight = weight;
     }
 
-    int hit(Fighter hitter) // Dövüşçünün nesnesi dövüşçüye atak özelliği aktarsın diye hit metotuna gönderildi
+    int hit(Fighter warriorToBeAttacked) // Dövüşçünün nesnesi dövüşçüye atak özelliği aktarsın diye hit metotuna gönderildi
     {
         System.out.println("============================================================");
         Random rand=new Random(); //
         int isWillBeDodge=rand.nextInt(0,10); // Savunma yapılıp yapılmaması için random sayı üretildi
-          if (hitter.healty>0)
+          if (warriorToBeAttacked.healty>0)
             {
-              System.out.println(this.name+" => "+hitter.name+" ÜZERİNE "+this.demage+" GÜCÜNDE ATAK YAPTI ");
+              System.out.println(this.name+" => "+warriorToBeAttacked.name+" ÜZERİNE "+this.demage+" GÜCÜNDE ATAK YAPTI ");
                 if (isWillBeDodge<=6 ) //Eğer üretilen sayı 6 dan küçükse savunma yapılıyor
                   {
-                     System.out.println(hitter.name+" den inanilmaz bir savunma..");
-                    return hitter.healty-(this.demage-dodge()); // savunma yapılırsa atak alan kişinin can durumu
+                     System.out.println(warriorToBeAttacked.name+" den inanilmaz bir savunma..");
+                    return warriorToBeAttacked.healty-(this.demage-dodge()); // savunma yapılırsa atak alan kişinin can durumu
                   }
                }
 
-         return hitter.healty-this.demage; //Savunma yapılmazsa atak alan kişinin can durumu
+         return warriorToBeAttacked.healty-this.demage; //Savunma yapılmazsa atak alan kişinin can durumu
     }
 
     int dodge() //Savunma gücünün ne kadar olduğunu belirlemek için oluşturulan metot
@@ -38,7 +39,5 @@ public class Fighter
         Random rand=new Random(); // Random savunma gücü belirleme
         return rand.nextInt(0, this.demage); // Savunma gücünü ilgili yere gönderme
     }
-
-
 
 }
