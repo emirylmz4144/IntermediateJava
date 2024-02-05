f3332b739e9abf6533fc2bc6a59f40f8672d8b2c
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-02-05 22:43:07 +0300

    command line and readme file added in FilterByFunctionalInterface

--- FilterByFunctionalInterface/src/KisiFiltreleme.java
+++ FilterByFunctionalInterface/src/KisiFiltreleme.java
@@ -1,3 +1,9 @@
+
+/**
+ * Metoda parametre olarak gelecek sınıfa ait özelliklerin
+ * her birine ait filtreleme metodu yapmak yerine functional interface
+ * ile sınıfa ait her özelliğe filtrelemeyi tek bir metot ile yapmak için
+ * oluşturulan interface **/
 @FunctionalInterface
 public interface KisiFiltreleme
 {
