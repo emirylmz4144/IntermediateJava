491be945b983581988722f9e01c3f280f0bd3f14
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-02-09 02:40:55 +0300

    Mikrotary Bird project updated and readme file added in

--- MikrotaryBird/src/Main.java
+++ MikrotaryBird/src/Main.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 public class Main
 {
-
-
-
-
+    static  Scanner input=new Scanner(System.in);
     public static void main(String[] args)
     {
 
-
-        Scanner input=new Scanner(System.in);
         int count =input.nextInt();// Dizinin boyutu alınıyor
         int [] ourArray=new int [count];//Dizi tanımlanıyor
 
+        //Kullanıcıdan alınan değerler diziye aktarılıyor
         for (int i=0;i<count;i++)
         {
-            //Kullanıcıdan alınan değerler diziye aktarılıyor
             int value=input.nextInt();
             ourArray[i]=value;
         }
+
 
         int [] mikrotaryBird=mikrotaryCounter(ourArray);//İlgili bilgiler ilgili fonksiyondadır.
 
@@ -34,23 +30,14 @@ public class Main
                 maxMikrotaryCounter=mikrotaryBird[i];// Şart sağlandıysa tekrar sayısını yeni max tekrar eden miktarı yapıyoruz
                 mikrotaryCounterValue=ourArray[i];// ve en fazla tekrar eden elemanı ilgili değişkene atıyoruz
             }
+            //Bir eleman en çok tekrar eden elemanlı aynı sayıda tekrar ediyorsa iki elemandan en küçük olanı alınır
             else if (mikrotaryBird[i]==maxMikrotaryCounter && ourArray[i]<mikrotaryCounterValue)
-            {
-                /*
-                 *Burası çok önemlidir çünki en dizinin ilgiili değişkeni en fazla tekrar etme miktarı ile aynı olabilir
-                 * bu durumda en küçük değeri en fazla tekrar eden eleman diye alacağımız için bu şart bizim kilit noktamızdır
-                 */
                 mikrotaryCounterValue=ourArray[i];
-            }
         }
 
         System.out.println(mikrotaryCounterValue);
 
-
     }
-
-
-
 
     public static int [] mikrotaryCounter(int [] ourArray)
     {
@@ -60,42 +47,23 @@ public class Main
             for(int j=0;j<ourArray.length;j++)
             {
                 if (isFind(ourArray[i],ourArray,i))//İlgili metotta anlatılmıştır
-                {
-                    continue;//İlgili metot sağlanıyorsa dizinin ilgili değişkeni es geçilir ve tekrar etme sayısı 0 atanır
-                }
+                  continue;//İlgili metot sağlanıyorsa dizinin ilgili değişkeni es geçilir ve tekrar etme sayısı 0 atanır
 
                 if (ourArray[i]==ourArray[j])
-                {
-                    /*
-                     *dizideki ilgili indisdeki değer daha önce sayaca eklenmediysse ve yine tekrar edildiyse sayaç artar
-                     */
-                    migrotaryBird[i]++;
+                    migrotaryBird[i]++;//dizideki ilgili indisdeki değer daha önce sayaca eklenmediysse ve yine tekrar edildiyse sayaç artar
 
-                }
             }
         }
 
         return migrotaryBird;
     }
-
-
-
-
+    //Gelen elemanıın o ana kadar kontrol edilip edilmediği sağlanır
     public static boolean isFind(int value,int array[],int tempCounter)
     {
-        /*
-         *Bu kısımda kilit noktalar arasınd yer almaktaddır dizinin i.elemanı dizi ve i sayısı gönderilir
-         *ve for döngüsü i sayısını almadan burası çok önemli ^^İ SAYISINI ALMADAN^^ o sayıya kadar olan
-         * indisdeki değişkenleri dizinin i.elemanı ile kontrol eder ve eğer eşitlerse zaten o dizi sayaca
-         * eklenmiştir demektir true değer döndererek ilgili indisin zaten daha önce sayaca dahil edildiğini
-         * belirtir ve böylece sayaca dahil olmaz..
-         */
         for (int i=0;i<tempCounter;i++)
         {
             if (value==array[i])
-            {
                 return true;
-            }
         }
 
         return false;
